74e284d987f1767cc0c538fdce8c8f4077ff4fa9
albert camacho <camacho.albert82@gmail.com>
2019-07-11 19:42:17 -0400

    Added locators and methods in Header page object

--- src/main/java/com/jalasoft/sdfc/pages/Header.java
+++ src/main/java/com/jalasoft/sdfc/pages/Header.java
@@ -1,16 +1,34 @@
 package com.jalasoft.sdfc.pages;
 
+import com.jalasoft.sdfc.enums.HeaderTab;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class Header extends AbstractPage {
 
-    @FindBy(css = "a[href='/dashboard']")
-    private WebElement dashboardIcon;
+    // it should allows to find almost all the tab items and they arrows in the header
+    private final static String TAB_NAME = "//div[@class='bBottom']/descendant::a[@title='%s']";
+    private final static String TAB_NAME_ARROW = "%s/following::a/descendant::span[starts-with(text(),'%s')]/ancestor::a";
 
-    @FindBy(css = ".tc_projects_dropdown_link")
-    private WebElement pivotalTrackerDropdown;
+    @FindBy(css = ".appName span")
+    private WebElement applicationName;
+
+    public void clickOnTabName(HeaderTab tab) {
+        // Method used to click a TAB item in the header.
+        action.click(By.xpath(String.format(TAB_NAME, tab.getName())));
+    }
+
+    public void clickOnTabNameArrow(HeaderTab tab) {
+        // Method used to click in the arrow of a TAB item if exists in the header.
+        String tabName = String.format(TAB_NAME, tab.getName());
+        action.click(By.xpath(String.format(TAB_NAME_ARROW, tabName, tab.getName())));
+    }
+
+    public String getApplicationName() {
+        // Returns the application name which is next to the AppLauncher button.
+        return action.getText(applicationName);
+    }
+
 
 }
