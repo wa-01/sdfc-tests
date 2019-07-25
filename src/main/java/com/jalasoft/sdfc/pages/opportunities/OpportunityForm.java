8fb9ee180fb17138836798aeec83dd99b491710f
albert camacho <camacho.albert82@gmail.com>
2019-07-25 02:12:46 -0400

    Update OpportunityForm with correct methods in the strategy map

--- src/main/java/com/jalasoft/sdfc/pages/opportunities/OpportunityForm.java
+++ src/main/java/com/jalasoft/sdfc/pages/opportunities/OpportunityForm.java
@@ -12,22 +12,22 @@ public class OpportunityForm extends BasicForm {
     @Override
     public void setDataInFormFields(Map<String, String> data) {
         Map<String, ISteps> strategyMap = new HashMap<>();
-        strategyMap.put("amount",() -> fillTextArea("Amount", data.get("amount")));
+        strategyMap.put("amount",() -> fillTextField("Amount", data.get("amount")));
         strategyMap.put("private",() -> setPrivate(data.get("private")));
-        strategyMap.put("close_date",() -> fillTextArea("Close Date", data.get("close_date")));
-        strategyMap.put("name",() -> fillTextArea("Opportunity Name", data.get("name")));
-        strategyMap.put("next_step",() -> fillTextArea("Next Step", data.get("next_step")));
+        strategyMap.put("close_date",() -> fillTextField("Close Date", data.get("close_date")));
+        strategyMap.put("name",() -> fillTextField("Opportunity Name", data.get("name")));
+        strategyMap.put("next_step",() -> fillTextField("Next Step", data.get("next_step")));
         strategyMap.put("account_name",() -> selectOptionInTextField("Account Name", data.get("account_name")));
         strategyMap.put("stage",() -> selectOptionInSelectField("Stage", data.get("stage")));
         strategyMap.put("type",() -> selectOptionInSelectField("Type", data.get("type")));
-        strategyMap.put("probability",() -> fillTextArea("Probability", data.get("probability")));
+        strategyMap.put("probability",() -> fillTextField("Probability", data.get("probability")));
         strategyMap.put("lead_source",() -> selectOptionInSelectField("Lead Source", data.get("lead_source")));
         strategyMap.put("primary_Campaign",() -> selectOptionInTextField("Primary Campaign Source", data.get("primary_Campaign")));
-        strategyMap.put("order_number",() -> fillTextArea("Order Number", data.get("order_number")));
-        strategyMap.put("main_competitor",() -> fillTextArea("Main Competitor", data.get("main_competitor")));
-        strategyMap.put("current_generator",() -> fillTextArea("Current Generator", data.get("current_generator")));
+        strategyMap.put("order_number",() -> fillTextField("Order Number", data.get("order_number")));
+        strategyMap.put("main_competitor",() -> fillTextField("Main Competitor", data.get("main_competitor")));
+        strategyMap.put("current_generator",() -> fillTextField("Current Generator", data.get("current_generator")));
         strategyMap.put("delivery_status",() -> selectOptionInSelectField("Delivery/Installation Status", data.get("delivery_status")));
-        strategyMap.put("tracking_number",() -> selectOptionInSelectField("Tracking Number", data.get("tracking_number")));
+        strategyMap.put("tracking_number",() -> fillTextField("Tracking Number", data.get("tracking_number")));
         strategyMap.put("description",() -> fillTextArea("Description", data.get("description")));
 
         Set<String> keys = data.keySet();
