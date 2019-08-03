d6596ab17a01cf2c09702de55f0cb5110cc7cb44
Edgar Quispe Arroyo <Edgar Quispe@jalasoft.local>
2019-08-02 21:27:18 -0400

    refactoring accounts

--- src/main/java/com/jalasoft/sdfc/pages/accounts/AccountsPage.java
+++ src/main/java/com/jalasoft/sdfc/pages/accounts/AccountsPage.java
@@ -1,4 +1,16 @@
 package com.jalasoft.sdfc.pages.accounts;
 
-public class AccountsPage {
+import com.jalasoft.sdfc.pages.AbstractPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AccountsPage extends AbstractPage {
+
+    @FindBy(css = "a[title='New'][role='button']")
+    private WebElement newButton;
+    public void clickNewButton() {
+        action.click(newButton);
+    }
 }
